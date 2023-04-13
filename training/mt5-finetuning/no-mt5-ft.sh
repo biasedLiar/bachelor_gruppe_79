e4b4665fc7e50710f487d8dd11574c30d6a36141
BiasedLiar <elelmtg@gmail.com>
2023-04-13 21:24:58 +0200

    Working on mt5

---
+++ training/mt5-finetuning/no-mt5-ft.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --partition=GPUQ
+#SBATCH --account=ie-idi
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:V10032:1
+#SBATCH --job-name="mt5-ft.py"
+#SBATCH --output=log.out
+#SBATCH --mail-user=eliaseb@stud.ntnu.no
+#SBATCH --mail-type=ALL
+
+
+module purge
+module load Anaconda3/2022.05
+python no-mt5-ft.py
