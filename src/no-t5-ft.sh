3518124707e1f4124ed071b85d810db7464b33ce
Ask Røsand <askbr@stud.ntnu.no>
2023-04-25 21:34:44 +0200

    Removed old src folder

--- src/no-t5-ft.sh
+++ src/no-t5-ft.sh
@@ -3,9 +3,12 @@
 #SBATCH --account=ie-idi
 #SBATCH --time=02:00:00
 #SBATCH --gres=gpu:V10032:1
+#SBATCH --mem=12000
 #SBATCH --job-name="py"
-#SBATCH --output=lbm_cuda_not5ft3.out
+#SBATCH --output=log.out
 
 module purge
 module load Anaconda3/2022.05
-python no-t5-ft.py
+module load git-lfs/2.11.0
+python -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token('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')"
+python train.py -model_checkpoint north/t5_base_NCC_lm -model_type t5 -dataset_path BiasedLiar/nor_email_sum -save_online True
--- src/rg.py
+++ src/rg.py
@@ -1,18 +1,21 @@
 import spacy
 import evaluate
 
-nlp_nb = spacy.load("nb_core_news_sm")
+nlp_nb = spacy.load("nb_core_news_sm")  # remember to download: python -m spacy download nb_core_news_sm
 rouge_score = evaluate.load("rouge")
+
 
 def correctFormat(summary, max_sentences=None):
     """
     Corrects the format of a summary for evaluating with rouge scores. The correct format requires sentences to be
-    seperated by newlines.
+    seperated by newlines. Source: https://github.com/google-research/google-research/tree/master/rouge
 
-    :param summary: String containing a summary of multiple sentences or just a single sentence.
-    :param max_sentences: Maximum sentences for each summary, the remaining sentences will be ignored.
+    :param summary: (str) A summary of some text. The summary can have multiple sentences or just a single sentence.
+    :param max_sentences: (int) Maximum sentences for each summary, the remaining sentences will be ignored.
     :return: String with each sentence of the summary seperated with a newline.
     """
+    if summary == None:
+        summary = ""
     doc = nlp_nb(summary)
     sentences = [sent.text for sent in doc.sents]
 
@@ -23,10 +26,11 @@ def correctFormat(summary, max_sentences=None):
 
 
 def correctFormatList(summary_list, max_sentences=None):
-    """ See docs for correctFormat. This method does the same but with a list."""
+    """ See docs for correctFormat. This method does the same but with a list of summaries."""
     summary_list_corrected = []
 
     for i in range(len(summary_list)):
+
         summary_list_corrected.append(correctFormat(summary_list[i], max_sentences=max_sentences))
 
     return summary_list_corrected
@@ -34,10 +38,10 @@ def correctFormatList(summary_list, max_sentences=None):
 
 def compareRouge(label_summaries, pred_summaries):
     """
-    Gives rouge score for two lists of summaries on normal format (no newlines).
+    Gives rouge score for two lists of summaries on normal format. Summary lists does not need to be on correct format
     :param label_summaries: Label summaries
     :param pred_summaries: Predicted summaries
-    :return:
+    :return: Rouge scores
     """
     label_summaries_corrected = correctFormatList(label_summaries)
     pred_summaries_corrected = correctFormatList(pred_summaries)
@@ -46,18 +50,46 @@ def compareRouge(label_summaries, pred_summaries):
 
 
 # finner rouge score ved å kun velge lead anntall første setninger
-def getLeadRougeScore(dataset, lead):
-    lead_summaries = correctFormatList(dataset["text"], max_sentences=lead)
-    scores = rouge_score.compute(predictions=lead_summaries, references=dataset["label"])
+def getLeadRougeScore(dataset, text_name, label_name, lead):
+    """
+    Calculates the lead-X score for a dataset
+
+    :param dataset: Dataset used to compute lead score. NB! Not a DatasetDict but normally a test dataset
+    :param text_name: Name of the column with text being summarized in the dataset
+    :param label_name: Name of the column with labels (summaries) in the dataset
+    :param lead: Number of sentences in the lead summary, a summary consisting of the first x sentences of some text
+    :return: Rouge scores
+    """
+    lead_summaries = correctFormatList(dataset[text_name], max_sentences=lead)
+    scores = rouge_score.compute(predictions=lead_summaries, references=dataset[label_name])
     return scores
 
 
-def getPredictions(summarizer, dataset):
-    predictions = []
-    for i in range(len(dataset)):
-        predictions += summarizer(dataset[i]["text"])[0]["summary_text"]
+def getPredictions(summarizer, dataset, text_name):
+    """
+    Calculates summarization based on a summarizer object (from huggingface) for an entire dataset.
+
+    :param summarizer: Summarizer used when summarizing the text of the dataset
+    :param dataset: Dataset to summarize. NB! Not a DatasetDict but normally a test dataset
+    :param text_name: Name of the column with text being summarized in the dataset
+    :return: List of the predictions from the dataset, each entry is a String
+    """
+    texts = [i[text_name] for i in dataset]
+    predictions = summarizer(texts)
+    predictions = [i["summary_text"] for i in predictions]
     return predictions
 
-def getRougeScore(summarizer, dataset):
-    predictions = getPredictions(summarizer, dataset)
-    return compareRouge(dataset["label"], predictions)
+
+def getRougeScore(summarizer, dataset, text_name, label_name):
+    """
+    Calculates summarization based on a summarizer object (from huggingface) for an entire dataset.
+
+    :param summarizer: Summarizer used when summarizing the text of the dataset
+    :param dataset: Dataset to summarize. NB! Not a DatasetDict but normally a test dataset
+    :param text_name: Name of the column with text being summarized in the dataset
+    :param label_name: Name of the column with labels (summaries) in the dataset
+    :return: Rouge scores
+    """
+    # TODO: Dobbeltsjekke at rekkefølgen på predictions er samme som den i datasettet
+    predictions = getPredictions(summarizer, dataset, text_name)
+    return compareRouge(dataset[label_name], predictions)
