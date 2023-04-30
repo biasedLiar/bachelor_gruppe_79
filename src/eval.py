063c1dcda5bab30ee1126dc3433514e220d3a555
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2023-05-01 00:27:31 +0200

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,25 +1,16 @@
-import numpy as np
 from datasets import load_dataset
-from spacy import tokenizer
-from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer, GPTJForCausalLM, AutoModel
+from transformers import pipeline
 import sys
 import utils
 import logging
 import rg
 
-#Base config settings
+# Base config settings
 tconf = {
-    "model_checkpoint": None,
-    "load_from_hf": True,
-    "model_type": None,
+    "model_checkpoint": None,  # must be from hf hub
     "dataset_path": None,
     "dataset_type": "hf",  # hf indicates data is loaded from huggingface
-    "max_input_length": 512,
-    "max_target_length": 256,
-    "batch_size": 8,
-    "num_train_epochs": 8,
-    "lr": 5.6e-5,
-    "weight_decay": 0.01,
+    "max_summary_length": 512,
     "input_name": "text",
     "label_name": "label",
     "gold_label_name": "goldlabel",
@@ -27,49 +18,31 @@ tconf = {
 }
 
 # Update config based on input
-args = sys.argv
-for i in range(len(args)):
-    if args[i][0] == '-' and args[i][1] != '-':
-        val = args[i + 1]
-        if utils.is_int(val):
-            val = int(val)
-        elif utils.is_float(val):
-            val = float(val)
-        tconf[args[i][1:]] = val
+utils.update_dict_from_args(tconf, sys.argv)
 
 
-#Load dataset
-print("dataset")
-print(tconf["dataset_path"])
-no_summary_dataset = load_dataset(tconf["dataset_path"])
+# Set logging configuration
+logging.basicConfig(
+    level=utils.get_logging_level(tconf["logging_level"]),
+    format="%(asctime)s %(levelname)s | %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S"
+)
 
-#Load model
-hub_model_id = tconf["model_checkpoint"]
-summarizer = pipeline("summarization", model=hub_model_id)
+# Validate required configuration fields
+if tconf["model_checkpoint"] is None or tconf["dataset_path"] is None:
+    logging.error("Some of the required fields (model_checkpoint, model_type and dataset_path) is not set.")
+    exit()
 
-# Create summaries of dataset with model
-texts = [row["text"] for row in no_summary_dataset["test"]]
-predictions = summarizer(texts)
-for i in predictions:
-    i = i["summary_text"].replace("<extra_id_0>", "")
-pred = [row["summary_text"] for row in predictions]
+# Establishing the dataset
+dataset = load_dataset(tconf["dataset_path"])
+logging.info(f"Dataset {tconf['dataset_path']} was loaded from HuggingFace.")
 
+# Initialize summarizer
+summarizer = pipeline("summarization", model=tconf["model_checkpoint"])
 
-
-
-#Create standard to measure summaries against.
-gold_labels = []
-labels = []
-for row in no_summary_dataset["test"]:
-    gold_labels.append(row["goldlabel"])
-    labels.append(row["label"])
-
-#Print results.
-print("Score vs human generated gold labels:")
-print(rg.compareRouge(gold_labels, pred))
-print()
-print("Score vs gpt-generated labels:")
-print(rg.compareRouge(labels, pred))
-
-
-
+print("===================================")
+print("SCORE AGAINST GOLD LABELS:")
+print(rg.getRougeScore(summarizer, dataset["test"], tconf["input_name"], tconf["gold_label_name"], tconf["max_summary_length"]))
+print("===================================")
+print("SCORE AGAINST HUMAN LABELS:")
+print(rg.getRougeScore(summarizer, dataset["test"], tconf["input_name"], tconf["label_name"], tconf["max_summary_length"]))
